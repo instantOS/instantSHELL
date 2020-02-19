e010ce0564fa6fdab5c8ade2a79c26ba27bf8303
paperbenni <paperbenni@gmail.com>
2020-02-19 23:26:52 +0100

    add autojump path to plugin

--- install.sh
+++ install.sh
@@ -117,8 +117,9 @@ setup_files() {
 
 setup_config() {
 	echo "editing config files"
-	sed -i 's/ZSH_TEME=.*/ZSH_THEME="agnoster"\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOCONNECT=false/g' ~/.zshrc
+	sed -i 's/ZSH_THEME=.*/ZSH_THEME="agnoster"\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOCONNECT=false/g' ~/.zshrc
 	sed -i 's/^plugins=.*/plugins=(git tmux common-aliases archlinux autojump instantos)/g' ~/.zshrc
+	sed -i 's~with MacPorts~with MacPorts\n/usr/share/autojump/autojump.zsh # pacman installation~g'
 }
 
 main() {

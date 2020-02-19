41beac08f49b378b0bb4e5bf8d179983e3f8281d
paperbenni <paperbenni@gmail.com>
2020-02-19 18:44:27 +0100

    remove shell changer

--- install.sh
+++ install.sh
@@ -122,69 +122,6 @@ setup_config() {
 	echo 'prompt_context(){}' >>~/.zshrc
 }
 
-setup_shell() {
-	# Skip setup if the user wants or stdin is closed (not running interactively).
-	if [ $CHSH = no ]; then
-		return
-	fi
-
-	# If this user's login shell is already "zsh", do not attempt to switch.
-	if [ "$(basename "$SHELL")" = "zsh" ]; then
-		return
-	fi
-
-	# If this platform doesn't provide a "chsh" command, bail out.
-	if ! command_exists chsh; then
-		cat <<-EOF
-			I can't change your shell automatically because this system does not have chsh.
-			${BLUE}Please manually change your default shell to zsh${RESET}
-		EOF
-		return
-	fi
-
-	echo "${BLUE}Time to change your default shell to zsh:${RESET}"
-
-	# Prompt for user choice on changing the default login shell
-	printf "${YELLOW}Do you want to change your default shell to zsh? [Y/n]${RESET} "
-	read opt
-	case $opt in
-	y* | Y* | "") echo "Changing the shell..." ;;
-	n* | N*)
-		echo "Shell change skipped."
-		return
-		;;
-	*)
-		echo "Invalid choice. Shell change skipped."
-		return
-		;;
-	esac
-
-	# Check if we're running on Termux
-	case "$PREFIX" in
-	*com.termux*)
-		termux=true
-		zsh=zsh
-		;;
-	*) termux=false ;;
-	esac
-
-	# We're going to change the default shell, so back up the current one
-	if [ -n "$SHELL" ]; then
-		echo $SHELL >~/.shell.pre-oh-my-zsh
-	else
-		grep "^$USER:" /etc/passwd | awk -F: '{print $7}' >~/.shell.pre-oh-my-zsh
-	fi
-
-	# Actually change the default shell to zsh
-	if ! chsh -s "$zsh"; then
-		error "chsh command unsuccessful. Change your default shell manually."
-	else
-		export SHELL="$zsh"
-		echo "${GREEN}Shell successfully changed to '$zsh'.${RESET}"
-	fi
-
-	echo
-}
 
 main() {
 	# Run as unattended if stdin is closed
@@ -222,7 +159,6 @@ main() {
 
 	setup_files
 	setup_zshrc
-	setup_shell
 	setup_config
 
 	printf "$GREEN"

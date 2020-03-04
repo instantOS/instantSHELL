6eb613c7dd21f4a6b4331e83faae44b4cd4b4f26
paperbenni <paperbenni@gmail.com>
2020-03-04 14:15:49 +0100

    disable auto zsh update

--- install.sh
+++ install.sh
@@ -123,9 +123,10 @@ setup_files() {
 
 setup_config() {
 	echo "editing config files"
-	sed -i 's/ZSH_THEME=.*/ZSH_THEME="instantos"\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOCONNECT=false/g' ~/.zshrc
-	sed -i 's/^plugins=.*/plugins=(git tmux common-aliases archlinux autojump instantos)/g' ~/.zshrc
-	sed -i 's~with MacPorts~with MacPorts\n/usr/share/autojump/autojump.zsh # pacman installation~g'
+	sed -i 's/ZSH_THEME=.*/ZSH_THEME="instantos"\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOCONNECT=false/g' ~/.zshrc # autostart tmux
+	sed -i 's/^plugins=.*/plugins=(git tmux common-aliases archlinux autojump instantos)/g' ~/.zshrc                                       # preenable plugins
+	sed -i 's~with MacPorts~with MacPorts\n/usr/share/autojump/autojump.zsh # pacman installation~g' ~/.zshrc                              # autojump in custom location
+	sed -i 's~# DISABLE_AUTO_UPDATE~DISABLE_AUTO_UPDATE~g' ~/.zshrc                                                                        # incompatible with custom stuff
 }
 
 main() {
